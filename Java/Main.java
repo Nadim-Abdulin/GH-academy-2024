1214e87136e0d1dc0219f58e53cc90954bba9100
Nadim Abdulin <nadimabdulin@gmail.com>
2024-11-24 19:34:37 -0500

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -3,7 +3,7 @@ package PrimeNumber;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click t he <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
