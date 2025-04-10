87778126c9258bee1411380476be4e0ff475d44f
Nadim Abdulin <nadimabdulin@gmail.com>
2025-04-10 13:27:21 -0500

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -2,8 +2,8 @@
 
 import java.util.Scanner;
 
-//TIP To  <b>Run</b > code press  <shortcut actionId="Run"/> or
-// click  the  <icon src="AllIcons.Actions.Execute"/>   icon  in  the g utter.
+//TIP To <b>Run</b > code press  <shortcut actionId="Run"/> or
+// click the  <icon src="AllIcons.Actions.Execute"/>   icon  in  the g utter.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
